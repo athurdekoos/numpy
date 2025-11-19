6194104d4fd6dfc4a9c55921933b2407b23c467e
Amelia Thurdekoos <athurdekoos@quansight.com>
2025-11-19 14:26:06 -0800

    WIP: Api works

--- numpy/_core/src/multiarray/halffloat/static_nhalffloat.cpp
+++ numpy/_core/src/multiarray/halffloat/static_nhalffloat.cpp
@@ -1,6 +1,8 @@
-#include "static_nhalffloat.h"
+
+
 #include <iostream>
 #include "numpy/ndarraytypes.h"
+#include "static_nhalffloat.h"
 
 /*NUMPY_API
  * Test of static_nhalffloat.cpp
@@ -10,3 +12,11 @@ NpyHalffloat_test()
 {
     std::cout << "I am static_nhalffloat.cpp" << std::endl;
 }
+
+//make sure numpy itself is comming the c api 
+//add c++ tests 
+//new meson project can use the new c++ project correctly 
+//also have 
+//can't have people use import array
+//generate depricate warnings
+//add macro to generate warning - potentially - look back to numpy 2
--- numpy/_core/src/multiarray/halffloat/static_nhalffloat.h
+++ numpy/_core/src/multiarray/halffloat/static_nhalffloat.h
@@ -1,9 +1,13 @@
-#pragma once
-//TODO: Why is this named static? 
+//#pragma once
+
+
+//TODO: Why is this named static?
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+NPY_NO_EXPORT void 
+NpyHalffloat_test();
 
 #ifdef __cplusplus
 }
