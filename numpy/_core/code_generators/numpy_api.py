42b1d7eece844a93cbc903ff70daaf71cd73ab42
Amelia Thurdekoos <athurdekoos@quansight.com>
2025-11-25 15:02:15 -0800

    Float16 test with no MiniVersion

--- numpy/_core/code_generators/numpy_api.py
+++ numpy/_core/code_generators/numpy_api.py
@@ -406,7 +406,7 @@ multiarray_funcs_api = {
     # The actual public API for this is the inline function
     # `PyDataType_GetArrFuncs` checks for the NumPy runtime version.
     '_PyDataType_GetArrFuncs':                       (365,),
-    'NpyHalffloat_test':                             (369, MinVersion("2.0")),
+    'NpyHalffloat_test':                             (369, ),
     # End 2.0 API
     # NpyIterGetTransferFlags (slot 223) added.
     # End 2.3 API
